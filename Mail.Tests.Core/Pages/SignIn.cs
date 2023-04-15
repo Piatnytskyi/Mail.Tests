8894dcf0b2678c9517017a13596d58f5813060f1
Віталій Пятницький <Vitalii.Piatnytskyi.mPZIP.2022@edu.lpnu.ua>
2023-04-15 15:21:57 +0300

    Created SignIn page object

--- Mail.Tests.Core/Pages/SignIn.cs
+++ Mail.Tests.Core/Pages/SignIn.cs
@@ -4,23 +4,26 @@
     {
         private readonly WebDriver _webDriver;
 
-        private readonly By usernameBy = By.Name("user_name");
-        private readonly By passwordBy = By.Name("password");
-        private readonly By signinBy = By.Name("sign_in");
+        private readonly By _usernameBy = By.Name("loginfmt");
+        private readonly By _passwordBy = By.Name("passwd");
+        private readonly By _nextBy = By.Id("idSIButton9");
 
         public SignIn(WebDriver webDriver)
         {
-            this._webDriver = webDriver;
-            if (!webDriver.Title.Equals("Sign In Page"))
+            _webDriver = webDriver;
+            if (!webDriver.Url.Contains("login.live.com/login") || !webDriver.Url.Contains("outlook.live.com"))
                 throw new InvalidDataException("This is not Sign In Page, current page is: " + webDriver.Url);
         }
 
-        public Home loginValidUser(String userName, String password)
+        public Mail Login(string userName, string password)
         {
-            _webDriver.FindElement(usernameBy).SendKeys(userName);
-            _webDriver.FindElement(passwordBy).SendKeys(password);
-            _webDriver.FindElement(signinBy).Click();
-            return new Home(_webDriver);
+            _webDriver.FindElement(_usernameBy).SendKeys(userName);
+            _webDriver.FindElement(_nextBy).Click();
+
+            _webDriver.FindElement(_passwordBy).SendKeys(password);
+            _webDriver.FindElement(_nextBy).Click();
+
+            return new Mail(_webDriver);
         }
     }
 }
