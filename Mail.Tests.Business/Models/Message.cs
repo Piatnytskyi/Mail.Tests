c031e4ae94619e394604366b73a3a9d641df5771
Віталій Пятницький <Vitalii.Piatnytskyi.mPZIP.2022@edu.lpnu.ua>
2023-06-03 20:34:05 +0300

    Added test logger for JUnit format

--- Mail.Tests.Business/Models/Message.cs
+++ Mail.Tests.Business/Models/Message.cs
@@ -5,5 +5,10 @@
         public string To { get; set; }
         public string Subject { get; set; }
         public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return Subject;
+        }
     }
 }
